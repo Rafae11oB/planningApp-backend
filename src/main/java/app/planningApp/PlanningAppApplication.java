b00b11bc78186fb00cf27caf506986847bd22709
Rafaello <baimurzinrafael02@gmail.com>
2022-01-12 03:44:35 +0600

    Initial commit

---
+++ src/main/java/app/planningApp/PlanningAppApplication.java
@@ -0,0 +1,13 @@
+package app.planningApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanningAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanningAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/planningApp/PlanningAppApplicationTests.java
@@ -0,0 +1,13 @@
+package app.planningApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanningAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
